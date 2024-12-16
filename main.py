4bea5b5df5339fa6f7364700f2caa95afca17c4e
conghaowong <conghaowong@icloud.com>
2024-12-16 14:37:44 +0800

    Fix loading other models from the `main.py` in the root dir

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2024-11-05 15:29:32
 @LastEditors: Conghao Wong
-@LastEditTime: 2024-11-05 20:46:35
+@LastEditTime: 2024-12-16 11:28:38
 @Github: https://cocoon2wong.github.io
 @Copyright 2024 Conghao Wong, All Rights Reserved.
 """
@@ -11,6 +11,11 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.'))
+
+try:
+    import main
+except:
+    pass
 
 from playground import PlaygroundManager
 from qpid.args import Args
